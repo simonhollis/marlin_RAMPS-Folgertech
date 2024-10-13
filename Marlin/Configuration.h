495d4a714223dac0befa0328703d2d54019d3e48
Simon Hollis <simonhollis@southern-swallow.co.uk>
2024-10-13 22:48:02 +0100

    Alter bed sizes and probe offsets for new X-carriage

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -687,9 +687,9 @@
  *      O-- FRONT --+
  *    (0,0)
  */
-#define X_PROBE_OFFSET_FROM_EXTRUDER 0  // X offset: -left  +right  [of the nozzle]
-#define Y_PROBE_OFFSET_FROM_EXTRUDER 32  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -1.3   // Z offset: -below +above  [the nozzle] - decrease to lower extruders; increase to raise
+#define X_PROBE_OFFSET_FROM_EXTRUDER 28  // X offset: -left  +right  [of the nozzle]
+#define Y_PROBE_OFFSET_FROM_EXTRUDER 0  // Y offset: -front +behind [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -1.0   // Z offset: -below +above  [the nozzle] - decrease to lower extruders; increase to raise
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 8000
@@ -784,8 +784,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 155
-#define Y_BED_SIZE 175
+#define X_BED_SIZE 180
+#define Y_BED_SIZE 200
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -793,7 +793,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 155
+#define Z_MAX_POS 89
 
 /**
  * Software Endstops
@@ -928,10 +928,10 @@
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Set the boundaries for probing (where the probe can reach).
-  #define LEFT_PROBE_BED_POSITION 20 + X_PROBE_OFFSET_FROM_EXTRUDER // was 15
-  #define RIGHT_PROBE_BED_POSITION X_BED_SIZE + X_PROBE_OFFSET_FROM_EXTRUDER - 20 // was 170 
-  #define FRONT_PROBE_BED_POSITION 20 + Y_PROBE_OFFSET_FROM_EXTRUDER // was 20
-  #define BACK_PROBE_BED_POSITION Y_BED_SIZE - Y_PROBE_OFFSET_FROM_EXTRUDER // was 170
+  #define LEFT_PROBE_BED_POSITION 5 + X_PROBE_OFFSET_FROM_EXTRUDER // was 15
+  #define RIGHT_PROBE_BED_POSITION X_BED_SIZE - 5  // X_PROBE_OFFSET_FROM_EXTRUDER - 5 // was 170 
+  #define FRONT_PROBE_BED_POSITION 5 + Y_PROBE_OFFSET_FROM_EXTRUDER // was 20
+  #define BACK_PROBE_BED_POSITION Y_BED_SIZE - Y_PROBE_OFFSET_FROM_EXTRUDER - 5 // was 170
 
   // The Z probe minimum outer margin (to validate G29 parameters).
   #define MIN_PROBE_EDGE 10
